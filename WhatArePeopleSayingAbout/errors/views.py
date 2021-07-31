a948bbe9c83dfecc2a0b396d6f0ed7177ad8398c
Alessandro Alberga <ale.alberga1@gmail.com>
2021-08-01 00:44:51 +1000

    404 bug

--- WhatArePeopleSayingAbout/errors/views.py
+++ WhatArePeopleSayingAbout/errors/views.py
@@ -4,7 +4,14 @@ from django.template import RequestContext
 import random
 
 def handler404(request, *args, **argv):
-    response = render(request, 'app/errors/notfound.html', {})
+    rand = random.randint(1, 4)
+    notfound_page_class = 'notfound-page-' + str(rand)
+    template_name = 'app/errors/notfound.html'
+    response = render(
+        request, 
+        template_name, 
+        { 'notfound_page_class': notfound_page_class }
+    )
     response.status_code = 404
     return response
 
@@ -20,10 +27,4 @@ class NotFoundView(View):
     template_name = 'app/errors/notfound.html'
     
     def get(self, request):
-        rand = random.randint(1, 4)
-        notfound_page_class = 'notfound-page-' + str(rand)
-        return render(
-            request, 
-            self.template_name, 
-            { 'notfound_page_class': notfound_page_class }
-        )
+        return handler404(request)
